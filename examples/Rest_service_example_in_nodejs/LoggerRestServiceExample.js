4c8e921fd39145bca17a854270db4c8b0b246ce4
Udo Esser <ues@udo-esser.de>
2017-10-28 15:29:41 +0200

    Example enhanced

--- examples/Rest_service_example_in_nodejs/LoggerRestServiceExample.js
+++ examples/Rest_service_example_in_nodejs/LoggerRestServiceExample.js
@@ -6,23 +6,41 @@ var buffer = new Buffer();
 var app = express();
 
 app.get('/log', function(request, response) {
+  var now = new Date();
+  var day = now.getDate();
+  var month = now.getMonth() + 1;
+  var year = now.getFullYear();
+  var hour = now.getHours();
+  var minute = now.getMinutes();
+  var second = now.getSeconds();
+  var day0  = ((day < 10) ? "0" : "");
+  var month0  = ((month < 10) ? ".0" : "."); 
+  var hour0  = ((hour < 10) ? "0" : ""); 
+  var minute0  = ((minute < 10) ? ":0" : ":"); 
+  var second0  = ((second < 10) ? ":0" : ":"); 
+  var output = day0 + day + month0 + month + "." + year + " " + hour0 + hour + minute0 + minute + second0 + second;
+
   if (request.query.logLev) {
-    buffer = request.query.logLev;
-  }
-  else {
-    buffer = "UNDEF";
+    output += " - " + request.query.logLev;
   }
   if (request.query.logFct) {
-    buffer += " - " + request.query.logFct;
+    output += " - " + request.query.logFct;
   }
   if (request.query.logStr) {
-    buffer += " - " + request.query.logStr;
+    output += " - " + request.query.logStr;
   }
   if (request.query.logStrln) {
-    buffer += " - " + request.query.logStrln + "\n";
+    output += " - " + request.query.logStrln + "\n";
   }
 
+//  console.log("req.query: %s", JSON.stringify(req.query));
+
+  buffer = new Buffer.from(output);
   fs.appendFileSync("/var/log/logfile.log", buffer);
+
+//  console.log("File written: %s", buffer);
+
+  return res.send();
 });
 
 app.listen(3000);
